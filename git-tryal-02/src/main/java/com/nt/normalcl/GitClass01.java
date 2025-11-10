e54ef8dbc90d49c6291ddeec29a3b3e5cfe2b56e
Hritik688 <hritikshukla688@gmail.com>
2025-11-10 16:40:04 +0530

    first commit

---
+++ git-tryal-02/src/main/java/com/nt/normalcl/GitClass01.java
@@ -0,0 +1,11 @@
+package com.nt.normalcl;
+
+public class GitClass01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("jai shree ram");
+
+	}
+
+}
